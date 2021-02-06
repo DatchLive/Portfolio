729dd004db373bbe07e4821010af5d7aae8d3ea5
DatchLive <datchlivetv730@gmail.com>
2021-02-07 08:15:12 +0900

    🐛 💄

--- src/blog.js
+++ src/blog.js
@@ -19,28 +19,4 @@ async function createArticles() {
   document.getElementById('blogs').innerHTML = list;
 }
 
-if (document.getElementById('blogs')) {
-  createArticles();
-}
-
-//記事詳細ページ API
-
-document.getElementsByClassName('blog-article').onunload = function () {
-  let id = new URLSearchParams(location.search).get('id');
-
-  async function createTexts() {
-    const resText = await fetch(
-      `https://datchlivelog.microcms.io/api/v1/blog/${id}`,
-      {
-        headers: { 'X-API-KEY': 'ecd2e202-1e5d-4e56-9153-f45c5400d664' },
-      }
-    );
-    const jsonText = await resText.json();
-    const blogTitle = jsonText.title;
-    const blogContent = jsonText.text;
-
-    document.getElementById('titleElement').innerHTML = blogTitle;
-    document.getElementById('contentElement').innerHTML = blogContent;
-  }
-  createTexts();
-};
+createArticles();
