141454220ff69003151f7496b6efb0009fcc99b2
datchlive <71320906+DatchLive@users.noreply.github.com>
2021-02-17 23:02:28 +0900

    💄

--- src/dark.js
+++ src/dark.js
@@ -1,69 +1,75 @@
-'use strict';
+"use strict";
 
-const btn = document.querySelector('#toggle');
+const btn = document.querySelector("#toggle");
 const body = document.body.classList;
-const nav = document.getElementById('navigation').classList;
-const matchMedia = window.matchMedia('(max-width:1025px)');
+const nav = document.getElementById("navigation").classList;
+const matchMedia = window.matchMedia("(max-width:1025px)");
 
 //ローカルストレージ
-const keyLocalStorage = 'theme';
+const keyLocalStorage = "theme";
 const localTheme = localStorage.getItem(keyLocalStorage);
 
-btn.addEventListener('change', () => {
+btn.addEventListener("change", () => {
   if (btn.checked == true) {
     // ダークモード
-    body.remove('light-maintheme');
-    body.add('dark-maintheme');
-    localStorage.setItem(keyLocalStorage, '1');
+    body.remove("light-maintheme");
+    body.add("dark-maintheme");
+
+    localStorage.setItem(keyLocalStorage, "1");
     //レスポンシブ
     if (matchMedia.matches) {
-      nav.add('dark-navtheme');
-      nav.remove('light-navtheme');
+      nav.add("dark-navtheme");
+      nav.remove("light-navtheme");
     }
     //プロフィール
-    if (document.getElementById('aboutInfo')) {
-      document.getElementById('aboutInfo').classList.add('dark-proftheme');
-      document.getElementById('aboutInfo').classList.remove('light-proftheme');
+    if (document.getElementById("aboutInfo")) {
+      document.getElementById("aboutInfo").classList.add("dark-proftheme");
+      document.getElementById("aboutInfo").classList.remove("light-proftheme");
     }
+    // if (document.getElementsByClassName("modal-innercontainer")) {
+    //   document
+    //     .getElementsByClassName("modal-innercontainer")
+    //     .classList.add("dark-proftheme");
+    // }
   } else {
     // ライトモード
-    body.remove('dark-maintheme');
-    body.add('light-maintheme');
-    localStorage.setItem(keyLocalStorage, '2');
+    body.remove("dark-maintheme");
+    body.add("light-maintheme");
+    localStorage.setItem(keyLocalStorage, "2");
     if (matchMedia.matches) {
-      nav.add('light-navtheme');
-      nav.remove('dark-navtheme');
+      nav.add("light-navtheme");
+      nav.remove("dark-navtheme");
     }
-    if (document.getElementById('aboutInfo')) {
-      document.getElementById('aboutInfo').classList.add('light-proftheme');
-      document.getElementById('aboutInfo').classList.remove('dark-proftheme');
+    if (document.getElementById("aboutInfo")) {
+      document.getElementById("aboutInfo").classList.add("light-proftheme");
+      document.getElementById("aboutInfo").classList.remove("dark-proftheme");
     }
   }
 });
 
 const setThemeFile = function () {
-  if (localTheme === '1') {
-    body.remove('light-maintheme');
-    body.add('dark-maintheme');
+  if (localTheme === "1") {
+    body.remove("light-maintheme");
+    body.add("dark-maintheme");
     btn.checked = true;
     if (matchMedia.matches) {
-      nav.add('dark-navtheme');
-      nav.remove('light-navtheme');
+      nav.add("dark-navtheme");
+      nav.remove("light-navtheme");
     }
-    if (document.getElementById('aboutInfo')) {
-      document.getElementById('aboutInfo').classList.add('dark-proftheme');
-      document.getElementById('aboutInfo').classList.remove('light-proftheme');
+    if (document.getElementById("aboutInfo")) {
+      document.getElementById("aboutInfo").classList.add("dark-proftheme");
+      document.getElementById("aboutInfo").classList.remove("light-proftheme");
     }
   } else {
-    body.remove('dark-maintheme');
-    body.add('light-maintheme');
+    body.remove("dark-maintheme");
+    body.add("light-maintheme");
     if (matchMedia.matches) {
-      nav.add('light-navtheme');
-      nav.remove('dark-navtheme');
+      nav.add("light-navtheme");
+      nav.remove("dark-navtheme");
     }
-    if (document.getElementById('aboutInfo')) {
-      document.getElementById('aboutInfo').classList.add('light-proftheme');
-      document.getElementById('aboutInfo').classList.remove('dark-proftheme');
+    if (document.getElementById("aboutInfo")) {
+      document.getElementById("aboutInfo").classList.add("light-proftheme");
+      document.getElementById("aboutInfo").classList.remove("dark-proftheme");
     }
   }
 };
